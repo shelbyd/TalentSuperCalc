f4d84b4a9e4829c3755344e6a4020af5f9402799
Shelby Doolittle <shelby@shelbyd.com>
2022-12-28 09:32:14 -0500

    show basic state overlay

--- core.lua
+++ core.lua
@@ -17,10 +17,13 @@ function Main:OnInitialize()
 
     self:RegisterChatCommand('TSC', function() self:OpenConfig(); end);
 
-    self.buttonTextures = self.buttonTextures or {};
+    self.buttonOverlays = self.buttonOverlays or {};
     EventUtil.ContinueOnAddOnLoaded('Blizzard_ClassTalentUI', function()
         self:SetupHook();
     end);
+
+    Main:RegisterEvent("TRAIT_TREE_CHANGED", self.OnTraitTreeChanged)
+    Main:RegisterEvent("TRAIT_NODE_CHANGED", self.OnTraitNodeChanged)
 end
 
 function Main:IsTalentTreeViewerEnabled()
@@ -32,39 +35,92 @@ function Main:SetupHook()
     for talentButton in ClassTalentFrame.TalentsTab:EnumerateAllTalentButtons() do
         self:OnTalentButtonAcquired(talentButton);
     end
+
+    -- local configs = C_Traits.GetConfigsByType(1);
+    -- for i, config in ipairs(configs) do
+    --     DevTools_Dump(C_Traits.GetConfigInfo(config));
+    -- end
 end
 
-local YELLOW = {
-    r = 1,
-    g = 1,
-    b = 0,
-    a = 0.5,
-}
+local TRANSPARENT = { r = 0, g = 0, b = 0, a = 0 }
+local YELLOW = { r = 1, g = 1, b = 0, a = 0.5 }
+local GREEN = { r = 0, g = 1, b = 0, a = 0.5 }
+local RED = { r = 1, g = 0, b = 0, a = 0.5 }
 
 function Main:UpdateColors()
-    if(self.buttonTextures) then
-        for _, texture in pairs(self.buttonTextures) do
+    if(self.buttonOverlays) then
+        for _, texture in pairs(self.buttonOverlays) do
             texture:SetVertexColor(YELLOW.r, YELLOW.g, YELLOW.b, YELLOW.a);
         end
     end
 end
 
 local function UpdateNonStateVisualsHook(button)
-    Main.buttonTextures[button]:SetShown(true);
+    local nodeState = Main:NodeState(button.nodeID);
+
+    local color = TRANSPARENT;
+    local shown = true;
+    if nodeState == 'floating' then
+        color = YELLOW;
+    elseif nodeState == 'explicitly_selected' then
+        color = GREEN;
+    elseif nodeState == 'explicitly_rejected' then
+        color = RED;
+    elseif nodeState == 'inferred' then
+        shown = false;
+    else
+        error("Unrecognized node state " .. nodeState)
+    end
+
+    Main.buttonOverlays[button]:SetVertexColor(color.r, color.g, color.b, color.a);
+    Main.buttonOverlays[button]:SetShown(shown);
 end
 
 function Main:OnTalentButtonAcquired(button)
-    print(button)
-    if not self.buttonTextures[button] then
-        self.buttonTextures[button] = button:CreateTexture(nil, 'OVERLAY')
-        local texture = self.buttonTextures[button];
+    local nodeId = button.nodeID;
+
+    if not self.buttonOverlays[button] then
+        self.buttonOverlays[button] = button:CreateTexture(nil, 'OVERLAY')
+        local texture = self.buttonOverlays[button];
         texture:SetAllPoints(button);
         texture:SetTexture('Interface/Tooltips/UI-Tooltip-Background');
-        texture:SetVertexColor(YELLOW.r, YELLOW.g, YELLOW.b, YELLOW.a);
         texture:AddMaskTexture(button.IconMask);
         texture:Hide();
         hooksecurefunc(button, 'UpdateNonStateVisuals', UpdateNonStateVisualsHook);
     end
+
     UpdateNonStateVisualsHook(button);
 end
 
+-- State things
+
+function Main:NodeState(nodeId)
+    -- https://wowpedia.fandom.com/wiki/API_C_Traits.GetNodeInfo
+    local nodeInfo = C_Traits.GetNodeInfo(C_ClassTalents.GetActiveConfigID(), nodeId);
+
+    local ranks = nodeInfo.activeRank;
+    if ranks == 0 then
+        return 'explicitly_rejected';
+    end
+
+    for _, edge in ipairs(nodeInfo.visibleEdges) do
+        local otherNodeState = self:NodeState(edge.targetNode);
+        if otherNodeState == 'inferred' or otherNodeState == 'explicitly_selected' then
+            return 'inferred'
+        end
+    end
+
+    return 'explicitly_selected';
+end
+
+function Main:OnTraitTreeChanged(treeId)
+    DevTools_Dump(treeId)
+end
+
+function Main:OnTraitNodeChanged(nodeId)
+    print("OnTraitNodeChanged " .. nodeId)
+    local nodeInfo = C_Traits.GetNodeInfo(C_ClassTalents.GetActiveConfigID(), nodeId);
+    DevTools_Dump({
+        isAvailable = nodeInfo.isAvailable,
+    })
+end
