d001e5db8845c02b89eb7783f713676483e6bb48
Shelby Doolittle <shelby@shelbyd.com>
2022-12-28 08:25:19 -0500

    Remove TalentTreeTweaks things and highlight everything as floating

--- core.lua
+++ core.lua
@@ -1,164 +1,70 @@
-local name, TTT = ...;
+local name, TSC = ...;
 
 --@debug@
-_G.TalentTreeTweaks = TTT;
-if not _G.TTT then _G.TTT = TTT; end
+_G.TalentSuperCalc = TSC;
+if not _G.TSC then _G.TSC = TSC; end
 --@end-debug@
 
---- @class TalentTreeTweaks_Main
+--- @class TalentSuperCalc_Main
 local Main = LibStub('AceAddon-3.0'):NewAddon(name, 'AceConsole-3.0', 'AceHook-3.0', 'AceEvent-3.0');
 if not Main then return; end
-TTT.Main = Main;
+TSC.Main = Main;
 
 function Main:OnInitialize()
-    TalentTreeTweaksDB = TalentTreeTweaksDB or {};
-    self.db = TalentTreeTweaksDB;
+    TalentSuperCalcDB = TalentSuperCalcDB or {};
+    self.db = TalentSuperCalcDB;
     self.version = GetAddOnMetadata(name, "Version") or "";
-    self:InitDefaults();
-    TTT.Util:OnInitialize();
-    for moduleName, module in self:IterateModules() do
-        --- @type TalentTreeTweaks_Module
-        local module = module;
-        if self.db.modules[moduleName] == false then
-            module:Disable();
-        end
-    end
 
-    self:InitConfig();
+    self:RegisterChatCommand('TSC', function() self:OpenConfig(); end);
 
-    self:RegisterChatCommand('ttt', function() self:OpenConfig(); end);
-end
-
-function Main:InitDefaults()
-    local defaults = {
-        modules = {},
-        moduleDb = {},
-    };
-
-    for key, value in pairs(defaults) do
-        if self.db[key] == nil then
-            self.db[key] = value;
-        end
-    end
-end
-
-function Main:InitConfig()
-    local count = 1;
-    local function increment() count = count + 1; return count end;
-    self.options = {
-        type = 'group',
-        name = 'Talent Tree Tweaks',
-        desc = 'Various tweaks and improvements to the talent tree UI',
-        childGroups = 'tab',
-        args = {
-            version = {
-                order = increment(),
-                type = 'description',
-                name = 'Version: ' .. self.version,
-            },
-            modules = {
-                order = increment(),
-                type = 'group',
-                name = 'Modules',
-                childGroups = 'tree',
-                args = {
-                    desc = {
-                        order = increment(),
-                        type = 'description',
-                        name = 'This addon consists of a number of modules, each of which can be enabled or disabled, to fine-tune your experience.',
-                    },
-                },
-            }
-        }
-    };
-    local function GetFormattedModuleName(moduleName, disabledSuffix)
-        --- @type TalentTreeTweaks_Module
-        local module = self:GetModule(moduleName);
-
-        local prettyName = module.GetName and module:GetName() or moduleName;
-        if not self:IsModuleEnabled(moduleName) then
-            return RED_FONT_COLOR:WrapTextInColorCode(prettyName .. (disabledSuffix or ''));
-        end
-        return prettyName;
-    end
-
-    local defaultModuleOptions = {
-        type = 'group',
-        name = function(info)
-            local moduleName = info[#info];
-            return GetFormattedModuleName(moduleName);
-        end,
-        desc = function(info)
-            local moduleName = info[#info];
-            if not self:IsModuleEnabled(moduleName) then
-                return RED_FONT_COLOR:WrapTextInColorCode('Disabled');
-            end
-        end,
-        args = {
-            name = {
-                order = 1,
-                type = 'header',
-                name = function(info)
-                    local moduleName = info[#info - 1];
-                    return GetFormattedModuleName(moduleName);
-                end,
-            },
-            description = {
-                order = 2,
-                type = 'description',
-                name = function(info)
-                    --- @type TalentTreeTweaks_Module
-                    local module = self:GetModule(info[#info - 1]);
-                    return module.GetDescription and module:GetDescription() or '';
-                end,
-                hidden = function(info)
-                    return '' == info.option.name(info)
-                end,
-            },
-            enable = {
-                order = 3,
-                name = 'Enable',
-                desc = 'Enable this module',
-                type = 'toggle',
-                get = function(info) return self:IsModuleEnabled(info[#info - 1]); end,
-                set = function(info, enabled) self:SetModuleState(info[#info - 1], enabled); end,
-            },
-        },
-    };
-    for moduleName, module in self:IterateModules() do
-        --- @type TalentTreeTweaks_Module
-        local module = module;
-        local copy = CopyTable(defaultModuleOptions);
-        self.db.moduleDb[moduleName] = self.db.moduleDb[moduleName] or {};
-        local moduleOptions = module.GetOptions and module:GetOptions(copy, self.db.moduleDb[moduleName]) or copy;
-        moduleOptions.order = increment();
-        self.options.args.modules.args[moduleName] = moduleOptions;
-    end
-
-    self.configCategory = 'Talent Tree Tweaks';
-    LibStub('AceConfig-3.0'):RegisterOptionsTable(self.configCategory, self.options);
-    LibStub("AceConfigDialog-3.0"):AddToBlizOptions(self.configCategory);
-end
-
-function Main:OpenConfig()
-    Settings.OpenToCategory(self.configCategory);
-end
-
-function Main:SetModuleState(moduleName, enabled)
-    if enabled then
-        self:EnableModule(moduleName);
-    else
-        self:DisableModule(moduleName);
-    end
-    self.db.modules[moduleName] = enabled;
-end
-
-function Main:IsModuleEnabled(moduleName)
-    local module = self:GetModule(moduleName);
-
-    return module and module:IsEnabled() or false;
+    self.buttonTextures = self.buttonTextures or {};
+    EventUtil.ContinueOnAddOnLoaded('Blizzard_ClassTalentUI', function()
+        self:SetupHook();
+    end);
 end
 
 function Main:IsTalentTreeViewerEnabled()
     return GetAddOnEnableState(UnitName('player'), 'TalentTreeViewer') == 2;
 end
+
+function Main:SetupHook()
+    ClassTalentFrame.TalentsTab:RegisterCallback(TalentFrameBaseMixin.Event.TalentButtonAcquired, self.OnTalentButtonAcquired, self);
+    for talentButton in ClassTalentFrame.TalentsTab:EnumerateAllTalentButtons() do
+        self:OnTalentButtonAcquired(talentButton);
+    end
+end
+
+local YELLOW = {
+    r = 1,
+    g = 1,
+    b = 0,
+    a = 0.5,
+}
+
+function Main:UpdateColors()
+    if(self.buttonTextures) then
+        for _, texture in pairs(self.buttonTextures) do
+            texture:SetVertexColor(YELLOW.r, YELLOW.g, YELLOW.b, YELLOW.a);
+        end
+    end
+end
+
+local function UpdateNonStateVisualsHook(button)
+    Main.buttonTextures[button]:SetShown(true);
+end
+
+function Main:OnTalentButtonAcquired(button)
+    print(button)
+    if not self.buttonTextures[button] then
+        self.buttonTextures[button] = button:CreateTexture(nil, 'OVERLAY')
+        local texture = self.buttonTextures[button];
+        texture:SetAllPoints(button);
+        texture:SetTexture('Interface/Tooltips/UI-Tooltip-Background');
+        texture:SetVertexColor(YELLOW.r, YELLOW.g, YELLOW.b, YELLOW.a);
+        texture:AddMaskTexture(button.IconMask);
+        texture:Hide();
+        hooksecurefunc(button, 'UpdateNonStateVisuals', UpdateNonStateVisualsHook);
+    end
+    UpdateNonStateVisualsHook(button);
+end
+
